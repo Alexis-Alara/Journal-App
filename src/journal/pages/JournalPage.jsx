f71092373c1660b4086ac3d16f1282a44761d21f
Alexis-Alara <alexis.alaraa@gmail.com>
2023-06-27 17:24:56 -0600

    redux journalSlice and thunks

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,22 +1,43 @@
+import { useDispatch, useSelector } from "react-redux"
 import { AddOutlined } from "@mui/icons-material"
 import { IconButton, Typography } from "@mui/material"
+
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView, NothingSelectedView } from "../views"
+import { startNewNote } from "../../store/journal"
+
 
 export const JournalPage = () => {
+
+  const { isSaving, active } = useSelector( state => state.journal )
+
+  const dispatch = useDispatch()
+
+  const onClickNewNote = () => {
+    dispatch( startNewNote() )
+  }
+
   return (
     <JournalLayout>
       {/* <Typography>Journal Page</Typography> */}
 
+      {
+        (!!active)
+          ? <NoteView/>
+          : <NothingSelectedView/>
+      }
+
       {/* NothingSelected */}
-      { <NothingSelectedView/> } 
+      {/* { <NothingSelectedView/> }  */}
 
       <IconButton
+        onClick={ onClickNewNote }
         size='large'
+        disabled={ isSaving }
         sx={{
           color: 'white',
           backgroundColor: 'error.main',
-          'hover:': {backgroundColor: 'error.main', opacity: 0.9},
+          '&:hover': {backgroundColor: 'error.main', opacity: .6},
           position: 'fixed',
           right: 50,
           bottom: 50
